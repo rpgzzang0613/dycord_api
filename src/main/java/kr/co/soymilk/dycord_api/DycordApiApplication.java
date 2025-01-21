d5ea18a420534cdb042c9586d4bb031fc08a4068
bsho88 <rpgzzang0613@gmail.com>
2025-01-21 15:40:45 +0900

    init

---
+++ src/main/java/kr/co/soymilk/dycord_api/DycordApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.soymilk.dycord_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DycordApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DycordApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/soymilk/dycord_api/DycordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.soymilk.dycord_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DycordApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
