5fdb0cdbb7d504033b7e32ed82e2c59755db984f
bsho88 <rpgzzang0613@gmail.com>
2025-03-20 16:35:42 +0900

    이걸 왜만들었더라?

--- src/main/java/kr/co/soymilk/dycord_api/common/exception/InvalidJwtTokenException.java
+++
@@ -1,8 +0,0 @@
-package kr.co.soymilk.dycord_api.common.exception;
-
-public class InvalidJwtTokenException extends RuntimeException {
-
-    public InvalidJwtTokenException() {
-        super();
-    }
-}
