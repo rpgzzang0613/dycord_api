b67a6f2aeb5851b1c1ba95b742ca942aeebfb0a1
bsho88 <rpgzzang0613@gmail.com>
2025-03-26 16:43:44 +0900

    OIDC 메타데이터 dto 항목 추가 (사실상 사용하는건 jwks_url 뿐이긴 함)

--- src/main/java/kr/co/soymilk/dycord_api/member/dto/oauth2/oidc/OIDCMetaData.java
+++ src/main/java/kr/co/soymilk/dycord_api/member/dto/oauth2/oidc/OIDCMetaData.java
@@ -1,12 +1,32 @@
 package kr.co.soymilk.dycord_api.member.dto.oauth2.oidc;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OIDCMetaData {
 
+    private String issuer;
+    private String authorization_endpoint;
+    private String token_endpoint;
+    private String device_authorization_endpoint;
+    private String userinfo_endpoint;
+    private String revocation_endpoint;
     private String jwks_uri;
+    private List<String> token_endpoint_auth_methods_supported;
+    private List<String> subject_types_supported;
+    private List<String> id_token_signing_alg_values_supported;
+    Boolean request_uri_parameter_supported;
+    private List<String> response_types_supported;
+    private List<String> response_modes_supported;
+    private List<String> grant_types_supported;
+    private List<String> code_challenge_methods_supported;
+    private List<String> claims_supported;
+    private List<String> scopes_supported;
 
 }
