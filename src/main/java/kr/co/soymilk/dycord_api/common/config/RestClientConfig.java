5ae1a65f54d9e8e5406513b9e5aa328819dedf87
bsho88 <rpgzzang0613@gmail.com>
2025-03-27 13:27:49 +0900

    RestClient 컨피그 수정

--- src/main/java/kr/co/soymilk/dycord_api/common/config/RestClientConfig.java
+++ src/main/java/kr/co/soymilk/dycord_api/common/config/RestClientConfig.java
@@ -2,8 +2,11 @@ package kr.co.soymilk.dycord_api.common.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.core5.util.TimeValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -19,27 +22,39 @@ import java.util.concurrent.TimeUnit;
 public class RestClientConfig {
 
     private static final int CONNECTION_TIMEOUT_SEC = 10;
-    private static final int RESPONSE_TIMEOUT_SEC = 10;
+    private static final int SOCKET_TIMEOUT_SEC = 10;
+    private static final int IDLE_SEC = 15;
+
+    private static final int MAX_CONN_TOTAL = 100;
+    private static final int MAX_CONN_PER_ROUTE = 20;
 
     @Bean
-    public RestClient restClient(HttpClient httpClient) {
+    public RestClient restClient() {
         // SimpleClientRequestFactory를 사용할 경우 HttpStatus가 200이 아닐때 response body를 못읽음
         return RestClient.builder()
-                .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
+                .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient()))
                 .build();
     }
 
-    @Bean
-    public HttpClient httpClient() {
-        return HttpClients.custom()
-                .setDefaultRequestConfig(requestConfig())
+    private HttpClient httpClient() {
+        return HttpClientBuilder.create()
+                .setConnectionManager(connectionManager())
+                .evictIdleConnections(TimeValue.of(IDLE_SEC, TimeUnit.SECONDS))
                 .build();
     }
 
-    private RequestConfig requestConfig() {
-        return RequestConfig.custom()
-                .setConnectionRequestTimeout(CONNECTION_TIMEOUT_SEC, TimeUnit.SECONDS)
-                .setResponseTimeout(RESPONSE_TIMEOUT_SEC, TimeUnit.SECONDS)
+    private HttpClientConnectionManager connectionManager() {
+        return PoolingHttpClientConnectionManagerBuilder.create()
+                .setDefaultConnectionConfig(connectionConfig())
+                .setMaxConnTotal(MAX_CONN_TOTAL)
+                .setMaxConnPerRoute(MAX_CONN_PER_ROUTE)
+                .build();
+    }
+
+    private ConnectionConfig connectionConfig() {
+        return ConnectionConfig.custom()
+                .setConnectTimeout(CONNECTION_TIMEOUT_SEC, TimeUnit.SECONDS)
+                .setSocketTimeout(SOCKET_TIMEOUT_SEC, TimeUnit.SECONDS)
                 .build();
     }
 
